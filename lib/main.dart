41ec5c3c396b3c586be9d54e8e7b6718e31e9098
Josh Martinez <joshuaaaronmartinez@gmail.com>
2022-01-03 16:26:49 -0800

    Final commit.
    Added appIcon and ImageAssets. Changed the body and AppBar colors

--- lib/main.dart
+++ lib/main.dart
@@ -5,7 +5,16 @@ void main() {
   runApp(
     MaterialApp(
       home: Scaffold(
-        appBar: AppBar(),
+        backgroundColor: Colors.blueGrey,
+        appBar: AppBar(
+          title: const Center(child: Text('I Am Rich')),
+          backgroundColor: Colors.blueGrey[900],
+        ),
+        body: const Center(
+          child: Image(
+            image: AssetImage('images/diamond.png'),
+          ),
+        ),
       ),
     ),
   );
